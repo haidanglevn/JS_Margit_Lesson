047fd11368940f315f59282ac411374135a3b27b
silvercoyote97 <haidang.levn@gmail.com>
2022-08-16 18:26:14 +0300

    finish task military

--- 2nd week/Task_Military_Service/script.js
+++ 2nd week/Task_Military_Service/script.js
@@ -1,19 +1,14 @@
-let age = prompt("Please give us your age: ", "your age");
+function checkAge() {
+  let age = prompt("Please give us your age: ", "your age");
 
-switch (age) {
-  case age < 27:
-    alert("right age for military");
-    break;
-  case age < 41:
-    alert("You are in reserve");
-    break;
-  case age < 55:
-    alert("You are in backup reserve");
-    break;
-  case age >= 55:
-    alert("too aged");
-    break;
-  default:
-    alert("too young");
-    break;
+  if (age < 18) {
+    return console.log("too young");
+  } else if (age < 27) {
+    return console.log("nice age");
+  } else {
+    return console.log("too old");
+  }
 }
+
+console.log(age);
+checkAge();
