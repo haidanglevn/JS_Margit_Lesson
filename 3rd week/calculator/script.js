3f0d4571c7fc86ebe947641a5fc37f9442bb2168
Le Dang <s2200738@BC-C02J61CZQ6LR-MacBook-Air.local>
2022-08-24 12:31:46 +0300

    finish task 1 week 3 gas calc

--- 3rd week/calculator/script.js
+++ 3rd week/calculator/script.js
@@ -3,18 +3,18 @@ let gas, price, money;
 function calculate() {
   let price = document.getElementById("price").value;
   let money = document.getElementById("money").value;
-  gas = price / money;
-  console.log(gas);
+  gas = Math.floor(money/ price);
+  console.log("price: ", price,", money: ",money,"gas: ,",gas);
   if (gas>=10) {
     document.getElementById("result").innerHTML = "You have " +
     gas + " litres of gas and you can go, Hooray";
 } else {
-  document.getElementById("result").innerHTML = "You have " +
+  document.getElementById("result").innerHTML = "You have " 
     gas + " litres of gas and oops, you have to stay here muahahahahaha";
 }
   }
   
-console.log(gas);
+
 
 
 
