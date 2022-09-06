7a824848755179e52afcbc189f18dffae808fbc3
haidanglevn <haidang.levn@gmail.com>
2022-09-06 16:26:46 +0300

    BG generator: add css and js file with assets folder,fully working

--- 5th week/BG generator/script.js
+++ 5th week/BG generator/script.js
@@ -1,7 +1,17 @@
 let colorLeft, colorRight;
-const wrapperColor = (val) => {
+let gradientcode;
+let direction;
+
+const gradient = (val = "to top") => {
   colorLeft = document.getElementById("color1").value;
   colorRight = document.getElementById("color2").value;
   document.getElementById("color-wrapper1").style.backgroundColor = colorLeft;
   document.getElementById("color-wrapper2").style.backgroundColor = colorRight;
+  console.log(colorLeft, colorRight);
+  direction = val;
+  console.log(direction);
+  gradientcode = `linear-gradient(${direction}, ${colorLeft}, ${colorRight})`;
+  console.log(gradientcode);
+  document.getElementById("result").style.backgroundImage = gradientcode;
+  document.getElementById("cssCode").innerHTML = gradientcode;
 };
