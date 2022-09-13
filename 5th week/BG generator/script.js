bd8e95fc70fc460bb15d8484eff59931c4e063ae
haidanglevn <haidang.levn@gmail.com>
2022-09-13 11:05:00 +0300

    slight improvement

--- 5th week/BG generator/script.js
+++ 5th week/BG generator/script.js
@@ -4,11 +4,8 @@ const gradient = (val = "to top") => {
   colorRight = document.getElementById("color2").value;
   document.getElementById("color-wrapper1").style.backgroundColor = colorLeft;
   document.getElementById("color-wrapper2").style.backgroundColor = colorRight;
-  /* console.log(colorLeft, colorRight); */
   direction = val;
-  /* console.log(direction); */
   gradientcode = `linear-gradient(${direction}, ${colorLeft}, ${colorRight})`;
-  /*  console.log(gradientcode); */
   document.getElementById("result").style.backgroundImage = gradientcode;
   document.getElementById("cssCode").innerHTML = gradientcode;
 };
