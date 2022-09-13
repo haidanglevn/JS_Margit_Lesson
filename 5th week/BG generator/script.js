f896ad555c653a8f48c75702ac258e424823b2af
haidanglevn <haidang.levn@gmail.com>
2022-09-13 10:31:21 +0300

    clear white space in CSS

--- 5th week/BG generator/script.js
+++ 5th week/BG generator/script.js
@@ -1,17 +1,14 @@
-let colorLeft, colorRight;
-let gradientcode;
-let direction;
-
+let colorLeft, colorRight, gradientcode, direction;
 const gradient = (val = "to top") => {
   colorLeft = document.getElementById("color1").value;
   colorRight = document.getElementById("color2").value;
   document.getElementById("color-wrapper1").style.backgroundColor = colorLeft;
   document.getElementById("color-wrapper2").style.backgroundColor = colorRight;
-  console.log(colorLeft, colorRight);
+  /* console.log(colorLeft, colorRight); */
   direction = val;
-  console.log(direction);
+  /* console.log(direction); */
   gradientcode = `linear-gradient(${direction}, ${colorLeft}, ${colorRight})`;
-  console.log(gradientcode);
+  /*  console.log(gradientcode); */
   document.getElementById("result").style.backgroundImage = gradientcode;
   document.getElementById("cssCode").innerHTML = gradientcode;
 };
