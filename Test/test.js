946d30fa11ff1733f20c554a6e3bf70eda63e74f
haidanglevn <haidang.levn@gmail.com>
2022-09-14 15:01:21 +0300

    end of Sept 14th lesson: modal menu, mobile button is not done

--- Test/test.js
+++ Test/test.js
@@ -3,14 +3,42 @@ window.onscroll = function () {
 };
 
 const stickyHeader = () => {
-  let navbar = document.getElementById("navbar");
-  let sticky = window.pageYOffset;
-  console.log(sticky);
+  let header = document.getElementById("header");
+  let backToTop = document.getElementById("back_to_top");
   if (window.pageYOffset >= 80) {
-    navbar.classList.add("sticky");
+    header.classList.add("sticky");
+    backToTop.style.display = "block";
   } else {
-    navbar.classList.remove("sticky");
+    header.classList.remove("sticky");
+    backToTop.style.display = "none";
   }
 };
 
-const openModal = () => {};
+let modal = document.getElementById("modal");
+let body = document.body;
+const openModal = () => {
+  modal.style.display = "block";
+  body.style.overflow = "hidden"; /* block body scroll  */
+
+  /* when user click outside of modal, close the modal  */
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      body.style.overflow = "auto";
+    }
+  };
+};
+const closeModal = () => {
+  modal.style.display = "none";
+  body.style.overflow = "auto";
+};
+
+let navbar = document.getElementById("navbar");
+
+const mobMenu = () => {
+  if (navbar.classList.contains("responsive")) {
+    navbar.classList.remove("responsive");
+  }
+};
+
+mobButton.addEventListener("click", mobMenu);
