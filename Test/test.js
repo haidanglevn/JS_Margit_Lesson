b9e084b1c20c52ba29c0d26adb00fa535dcf3a1d
haidanglevn <haidang.levn@gmail.com>
2022-09-14 11:31:50 +0300

    portfolio DOM practices on test folder

--- Test/test.js
+++ Test/test.js
@@ -1,33 +1,16 @@
-class Car {
-  constructor(license, maker, model, owner, price, color) {
-    this.license = license;
-    this.maker = maker;
-    this.model = model;
-    this.owner = owner;
-    this.price = price;
-    this.color = color;
-  }
-}
-let car1 = new Car("ABC123", "Toyota", "Corolla", "Dang", "10000€", "black");
-let car2 = new Car("TRA423", "Nissan", "Quasqai", "John", "7500€", "red");
-let car3 = new Car(
-  "IOO982",
-  "Mercedes-Benz",
-  "Vito",
-  "Ashley",
-  "15000€",
-  "white"
-);
-let carList = [];
-carList.push(car1, car2, car3);
+window.onscroll = function () {
+  stickyHeader();
+};
 
-function check(license) {
-  for (let i = 0; i < carList.length; i++) {
-    if (carList[i].license == "a") {
-      console.log("Found");
-    } else {
-      console.log("Not found");
-    }
+const stickyHeader = () => {
+  let navbar = document.getElementById("navbar");
+  let sticky = window.pageYOffset;
+  console.log(sticky);
+  if (window.pageYOffset >= 80) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
   }
-}
-check();
+};
+
+const openModal = () => {};
