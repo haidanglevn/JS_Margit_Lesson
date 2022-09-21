dce5b99960889c0c1019024cd864b5967551f56b
haidanglevn <haidang.levn@gmail.com>
2022-09-21 11:04:05 +0300

    speed game update: circle was clicked and random number

--- 7th week Speed Game/script.js
+++ 7th week Speed Game/script.js
@@ -1,14 +1,34 @@
 const startButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
-const circle = document.querySelectorAll(".circle");
-
-console.log(circle);
+const circles = document.querySelectorAll(".circle");
+let score = 0;
 
 const startGame = () => {
   console.log("game started");
+  setTimeout(randomNumber(), 20000);
 };
+const randomNumber = () => {
+  let x = Math.floor(Math.random() * 4 + 1);
+  console.log("random number is ", x);
+};
+
 const endGame = () => {
+  openModal();
   console.log("game ended");
+  score = 0;
+  document.getElementById("score").innerHTML = score;
+};
+
+/* find which circle was clicked */
+circles.forEach((circle, i) => {
+  circle.addEventListener("click", () => clickCircle(i));
+});
+
+const clickCircle = (i) => {
+  score += 1;
+  console.log("score =", score);
+  document.getElementById("score").innerHTML = score;
+  console.log("circle clicked was: ", i + 1);
 };
 
 startButton.addEventListener("click", startGame);
