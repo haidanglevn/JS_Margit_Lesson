996381706abd6ccc3bb9f873f02af7abbb3285c1
haidanglevn <haidang.levn@gmail.com>
2022-09-21 14:57:57 +0300

    speed game ver1

--- 7th week Speed Game/script.js
+++ 7th week Speed Game/script.js
@@ -1,34 +1,56 @@
 const startButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
 const circles = document.querySelectorAll(".circle");
+const result = document.getElementById("result");
 let score = 0;
+let gameRunning, random, chosen, i;
 
+/* start the game */
 const startGame = () => {
+  document.getElementById("title").innerHTML = "CATCH THE EVIL PUMPKIN !!!";
   console.log("game started");
-  setTimeout(randomNumber(), 20000);
+  gameRunning = setInterval(randomNumber, 1000);
 };
+
+/* get random number */
 const randomNumber = () => {
-  let x = Math.floor(Math.random() * 4 + 1);
-  console.log("random number is ", x);
+  random = Math.floor(Math.random() * 3);
+  console.log("random number is ", random);
+  /* change background */
+  console.log("click circle ", circles[`${random + 1}`], "now");
+  chosen = circles[random];
+  chosen.classList.add("active");
 };
 
-const endGame = () => {
-  openModal();
-  console.log("game ended");
-  score = 0;
-  document.getElementById("score").innerHTML = score;
-};
-
-/* find which circle was clicked */
+/* find which circle was clicked and check if right circle was clicked */
 circles.forEach((circle, i) => {
   circle.addEventListener("click", () => clickCircle(i));
 });
 
 const clickCircle = (i) => {
-  score += 1;
-  console.log("score =", score);
-  document.getElementById("score").innerHTML = score;
   console.log("circle clicked was: ", i + 1);
+  if (random == i) {
+    score += 1;
+    console.log("score =", score);
+    chosen.classList.remove("active");
+  } else {
+    console.log("wrong");
+    endGame();
+  }
+  document.getElementById("score").innerHTML = score;
+};
+
+/* end the game */
+const endGame = () => {
+  openModal();
+  chosen.classList.remove("active");
+  result.textContent = `You get ${score}! Great job !!!!`;
+  console.log("game ended");
+  clearInterval(gameRunning);
+};
+
+const reset = () => {
+  window.location.reload();
 };
 
 startButton.addEventListener("click", startGame);
@@ -52,4 +74,5 @@ const openModal = () => {
 const closeModal = () => {
   modal.style.display = "none";
   body.style.overflow = "auto";
+  reset();
 };
