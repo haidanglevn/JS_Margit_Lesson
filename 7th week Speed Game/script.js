35ba51c68d33d669f7c9b86a977dc04916f7b65b
haidanglevn <haidang.levn@gmail.com>
2022-09-22 14:58:57 +0300

    add lives

--- 7th week Speed Game/script.js
+++ 7th week Speed Game/script.js
@@ -2,8 +2,11 @@ const startButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
 const circles = document.querySelectorAll(".circle");
 const result = document.getElementById("result");
+const hearts = document.querySelectorAll(".heart");
 let score = 0;
 let random = 0;
+let lives = 4;
+let dedCount = 0;
 let gameRunning, chosen, i;
 let scored = false;
 let firstRound = true;
@@ -15,31 +18,34 @@ const startGame = () => {
   gameRunning = setInterval(newRound, 1000);
 };
 
-/* get random number */
 const newRound = () => {
-  /* losing condition: no click */
-  if (firstRound == false && scored == false) {
+  if (lives <= 0) {
     endGame();
-  } else {
-    scored = false;
-    random1 = Math.floor(Math.random() * 3);
-    console.log("random number is ", random1);
-    /* make sure the next number is different then the last */
-    while (random1 == random) {
-      random1 = Math.floor(Math.random() * 3);
-    }
-    random = random1;
-
-    /* change background */
-    circles.forEach((circle) => {
-      if (circle.classList.contains("active")) {
-        circle.classList.remove("active");
-      }
-    });
-    chosen = circles[random];
-    chosen.classList.add("active");
   }
+  /* get random number */
+  random1 = Math.floor(Math.random() * 3);
+  console.log("random number is ", random1);
+  /* make sure the next number is different then the last */
+  while (random1 == random) {
+    random1 = Math.floor(Math.random() * 3);
+  }
+  random = random1;
+  /* change background */
+  circles.forEach((circle) => {
+    if (
+      circle.classList.contains("active") ||
+      circle.classList.contains("glow")
+    ) {
+      circle.classList.remove("active");
+      circle.classList.remove("glow");
+    }
+  });
+  chosen = circles[random];
+  chosen.classList.add("active");
+  noClick();
   firstRound = false;
+  document.getElementById("lives").innerHTML = lives;
+  console.log("current lives are ", lives);
 };
 
 /* find which circle was clicked and check if right circle was clicked */
@@ -52,12 +58,28 @@ const clickCircle = (i) => {
     score += 1;
     console.log("score =", score);
     scored = true;
+    chosen.classList.add("glow");
     /* add clicked pumpkin asset */
   } else {
     console.log("wrong");
-    endGame();
+    minusLife();
   }
   document.getElementById("score").innerHTML = score;
+};
+/* losing condition: no click */
+const noClick = () => {
+  if (firstRound == false && scored == false) {
+    minusLife();
+    console.log("No click, minus 1 life.", lives, " remaining");
+  }
+};
+
+/* when minus one life */
+const minusLife = () => {
+  lives -= 1;
+  dedCount += 1;
+  hearts[hearts.length - dedCount].classList.add("ded");
+  console.log("current lives are ", lives);
 };
 
 /* end the game */
@@ -82,7 +104,6 @@ let body = document.body;
 const openModal = () => {
   modal.style.display = "block";
   body.style.overflow = "hidden"; /* block body scroll  */
-
   /* when user click outside of modal, close the modal  */
   window.onclick = function (event) {
     if (event.target == modal) {
@@ -91,6 +112,7 @@ const openModal = () => {
     }
   };
 };
+
 const closeModal = () => {
   modal.style.display = "none";
   body.style.overflow = "auto";
