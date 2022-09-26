b96076fb538df347344992923f3f2dd817dcdc7b
haidanglevn <haidang.levn@gmail.com>
2022-09-27 01:00:07 +0300

    #

--- 7th week Speed Game/script.js
+++ 7th week Speed Game/script.js
@@ -1,3 +1,4 @@
+const resetButton = document.getElementById("setting-logo");
 const startButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
 const circles = document.querySelectorAll(".circle");
@@ -7,21 +8,51 @@ let score = 0;
 let random = 0;
 let lives = 4;
 let dedCount = 0;
-let gameRunning, chosen, i;
+let gameRunning, chosen, i, random1;
 let scored = false;
 let firstRound = true;
+let round = 0;
+let pace = 900;
+
+const settingToggle = () => {
+  let settingpage = document.getElementById("setting");
+  let playground = document.getElementById("playground");
+  if (settingpage.style.display === "none") {
+    settingpage.style.display = "block";
+    playground.style.display = "none";
+  } else {
+    settingpage.style.display = "none";
+    playground.style.display = "block";
+  }
+};
+const difficultyChange = (value) => {
+  if (value == "easy") {
+    pace = 1200;
+    document.getElementById("difficulty").innerHTML = "Easy";
+    console.log(pace);
+  } else if (value == "medium") {
+    pace = 900;
+    document.getElementById("difficulty").innerHTML = "Medium";
+    console.log(pace);
+  } else {
+    pace = 700;
+    document.getElementById("difficulty").innerHTML = "Hard";
+    console.log(pace);
+  }
+};
 
 /* start the game */
 const startGame = () => {
   document.getElementById("title").innerHTML = "CATCH THE EVIL PUMPKIN !!!";
   console.log("game started");
-  gameRunning = setInterval(newRound, 1000);
+  console.log("pace = ", pace);
+  gameRunning = setInterval(newRound, pace);
 };
 
 const newRound = () => {
-  if (lives <= 0) {
-    endGame();
-  }
+  round += 1;
+  console.log(`-----------ROUND ${round}------------`);
+
   /* get random number */
   random1 = Math.floor(Math.random() * 3);
   console.log("random number is ", random1);
@@ -30,7 +61,7 @@ const newRound = () => {
     random1 = Math.floor(Math.random() * 3);
   }
   random = random1;
-
+  /* turn off classes after every round */
   circles.forEach((circle) => {
     if (
       circle.classList.contains("active") ||
@@ -40,14 +71,19 @@ const newRound = () => {
       circle.classList.remove("glow");
     }
   });
-
+  console.log("Is this first round? ", firstRound);
+  console.log("Have user scored?", scored);
   /* change background */
   chosen = circles[random];
   chosen.classList.add("active");
   noClick();
   firstRound = false;
+  scored = false;
   document.getElementById("lives").innerHTML = lives;
-  console.log("current lives are ", lives);
+  console.log("Health: ", lives);
+  if (lives <= 0) {
+    endGame();
+  }
 };
 
 /* find which circle was clicked and check if right circle was clicked */
@@ -58,21 +94,22 @@ circles.forEach((circle, i) => {
 const clickCircle = (i) => {
   if (random == i) {
     score += 1;
-    console.log("score =", score);
+    console.log("Right circle clicked, score =", score);
     scored = true;
     chosen.classList.add("glow");
     /* add clicked pumpkin asset */
   } else {
-    console.log("wrong");
+    console.log("Wrong circle clicked, (-1)");
     minusLife();
   }
   document.getElementById("score").innerHTML = score;
 };
+
 /* losing condition: no click */
 const noClick = () => {
   if (firstRound == false && scored == false) {
     minusLife();
-    console.log("No click, minus 1 life.", lives, " remaining");
+    console.log("No click (-1).", lives, "health remaining");
   }
 };
 
@@ -81,7 +118,7 @@ const minusLife = () => {
   lives -= 1;
   dedCount += 1;
   hearts[hearts.length - dedCount].classList.add("ded");
-  console.log("current lives are ", lives);
+  console.log("health decreased");
 };
 
 /* end the game */
@@ -99,6 +136,7 @@ const reset = () => {
 
 startButton.addEventListener("click", startGame);
 stopButton.addEventListener("click", endGame);
+resetButton.addEventListener("click", settingToggle);
 
 /* Modal */
 let modal = document.getElementById("modal");
