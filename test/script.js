3912e5f3bb5f9074172c23b8974c9b925c2171cd
haidanglevn <haidang.levn@gmail.com>
2022-09-27 22:23:13 +0300

    finish test func addScore

--- test/script.js
+++ test/script.js
@@ -1,36 +1,36 @@
 let leaderboard = document.querySelector(".leaderboard");
 let order = 0;
-let playerName = "Player";
-let score;
-
-const scores = [];
+let playerName;
+let score, i;
+let scoreList = [];
+class Score {
+  constructor(playerName, point) {
+    this.playerName = playerName;
+    this.point = point;
+  }
+}
 
 const addScore = () => {
-  order += 1;
-  score = Math.floor(Math.random() * 100);
-  scores.push(score);
-  console.log(scores);
-  push();
-};
-
-const push = () => {
-  newOrder = scores.reverse();
-  console.log("Sort: ", newOrder);
-  leaderboard.innerHTML += `<div class="attempt">${order}/${playerName}: ${score}</div>`;
-  console.log("Push done");
-};
-array.forEach(functionA());
-const array = [1, 3, 2, 5, 4];
-const arrayNew = [];
-console.log(array);
-console.log(array[1]);
-console.log(array.length);
-
-const functionA = () => {
-  for (i = 1; (i = array.length); i++) {
-    if (array[i] < array[i + 1]) {
-      arrayNew.push(array[i]);
+  playerName = document.getElementById("playerName").value;
+  point = document.getElementById("point").value;
+  let newScore = new Score(playerName, point);
+  scoreList.unshift(newScore);
+  console.table(scoreList);
+  console.log(scoreList[0].playerName);
+  const updateScore = () => {
+    let scoreBoard = document.querySelector(".leaderboard-content");
+    let list;
+    if (scoreList.length <= 5) {
+      list = `<div class="attempt">${scoreList[0].playerName}: ${scoreList[0].point} </div>`;
+      scoreBoard.innerHTML += list;
+    } else {
+      list = `<div class="attempt">${scoreList[4].playerName}: ${scoreList[4].point} </div>
+        <div class="attempt">${scoreList[3].playerName}: ${scoreList[3].point} </div>
+        <div class="attempt">${scoreList[2].playerName}: ${scoreList[2].point} </div>
+        <div class="attempt">${scoreList[1].playerName}: ${scoreList[1].point} </div>
+        <div class="attempt">${scoreList[0].playerName}: ${scoreList[0].point} </div>`;
+      scoreBoard.innerHTML = list;
     }
-  }
-  console.log(arrayNew);
+  };
+  updateScore();
 };
