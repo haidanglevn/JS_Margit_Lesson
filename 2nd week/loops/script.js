bf630cbc610058336c5ba376f6306274884447b8
haidanglevn <haidang.levn@gmail.com>
2022-10-05 12:10:48 +0300

    slight edit for period overview

--- 2nd week/loops/script.js
+++ 2nd week/loops/script.js
@@ -1,30 +1,24 @@
+// Make a program that asks ten numbers and in the end prints out two biggest numbers.
+
 let i, number;
-let smallest = Infinity;
-let a;
-function howManyNumber() {
-  a = parseInt(prompt("How many number you want to give?"));
-  console.log("You want to give ", a, " number(s)");
-  if (isNaN(a) || a < 1) {
-    console.log("You did not gave a correct number, try again");
-    a = parseInt(
-      prompt(
-        "You did not gave a correct number, try again. How many number you want to give?"
-      )
-    );
-  } else {
-    for (i = 1; i < a + 1; i++) {
-      askNumber();
-    }
-  }
-}
-// bug: cannot run if user give 2 consecutive wrong answer//
+let num1 = (num2 = -Infinity);
+
 function askNumber() {
-  number = parseInt(prompt("Give me the number?"));
-  console.log("Your ", i, " number is ", number);
-  if (number < smallest) {
-    smallest = number;
+  number = parseInt(prompt("Give me a number?"));
+  console.log("Your number is ", number);
+}
+for (i = 1; i < 11; i++) {
+  askNumber();
+  if (number > num1 && number > num2) {
+    num1 = num2;
+    num2 = number;
+    console.log("num1 =", num1, " and num2 =", num2);
+  } else if (number > num1 && number < num2) {
+    num1 = number;
+    console.log("num1 =", num1, " and num2 =", num2);
+  } else {
+    console.log("Nothing changes, num1 =", num1, " and num2 =", num2);
   }
 }
 
-howManyNumber();
-console.log("The smallest number is ", smallest);
+console.log("The two biggest numbers are ", num1, " and ", num2);
